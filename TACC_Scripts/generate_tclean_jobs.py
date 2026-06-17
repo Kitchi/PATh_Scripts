4d3843cf45d1c8388a1e2ce52e778d8acc7ff7c0
ssekhar <ssekhar@login1.vista.tacc.utexas.edu>
2026-06-17 09:47:40 -0500

    Fixes for TACC scripts

--- TACC_Scripts/generate_tclean_jobs.py
+++ TACC_Scripts/generate_tclean_jobs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import math
 
 from casatools import msmetadata
 
@@ -15,9 +16,10 @@ def parse_channels(channel_str, nchan):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Generate tclean_jobs.txt for tclean.orig.py')
+        description='Generate tclean_jobs.txt for tclean.py')
 
-    parser.add_argument('input_MS', type=str, help='Path to the Measurement Set')
+    parser.add_argument('input_MS', type=str, nargs='+',
+                        help='Path(s) to Measurement Set(s)')
     parser.add_argument('--field', type=int, default=1, help='Field index (default: 1)')
     parser.add_argument('--spws', type=str, default='all',
                         help='Comma-separated SPW indices, or "all" (default: all)')
@@ -36,17 +38,6 @@ if __name__ == '__main__':
                         help='Output filename (default: tclean_jobs.txt)')
     args = parser.parse_args()
 
-    msmd.open(args.input_MS)
-    spws = msmd.spwsforfield(args.field)
-    print(f"spws {spws}")
-    nchan_per_spw = {spw: msmd.nchan(spw) for spw in spws}
-    print(f"nchan_per_spw {nchan_per_spw}")
-    msmd.close()
-
-    if args.spws != 'all':
-        selected_spws = [int(s) for s in args.spws.split(',')]
-        spws = [s for s in spws if s in selected_spws]
-
     tclean_opts = (
         f"--field {args.field} "
         f"--gridder \"{args.gridder}\" "
@@ -59,19 +50,56 @@ if __name__ == '__main__':
     )
 
     lines = []
-    for spw in spws:
-        nchan = nchan_per_spw[spw]
-        channels = parse_channels(args.channels, nchan)
 
-        for chan in channels:
-            block_idx = chan // args.channel_chunks
-            line = (
-                f"python tclean.orig.py {args.input_MS} "
-                f"--spw {spw} --channel_number {block_idx} "
-                f"--nchannels {args.channel_chunks} "
-                f"{tclean_opts}"
-            )
-            lines.append(line)
+    # Collect metadata from all MS files
+    ms_metadata = {}
+    for ms_path in args.input_MS:
+        msmd.open(ms_path)
+        nf = msmd.nfields()
+        print(f"{ms_path}")
+        print(f"  nfields: {nf}")
+        for fid in range(nf):
+            print(f"    field {fid}: name={msmd.namesforfields(fid)}, spws={msmd.spwsforfield(fid)}")
+        spws = msmd.spwsforfield(args.field)
+        nchan_per_spw = {spw: msmd.nchan(spw) for spw in spws}
+        print(f"  -> Using field {args.field}: spws={spws}, nchan_per_spw={nchan_per_spw}")
+        msmd.close()
+        ms_metadata[ms_path] = {'spws': spws, 'nchan_per_spw': nchan_per_spw}
+
+    # Determine common SPWs across all MS files
+    all_spws = [set(md['spws']) for md in ms_metadata.values()]
+    common_spws = sorted(set.intersection(*all_spws))
+
+    if args.spws != 'all':
+        selected_spws = [int(s) for s in args.spws.split(',')]
+        common_spws = [s for s in common_spws if s in selected_spws]
+
+    # For each common SPW, find the max nchan across all MS files
+    ms_paths_str = ' '.join(args.input_MS)
+
+    for spw in common_spws:
+        nchan = max(md['nchan_per_spw'][spw] for md in ms_metadata.values())
+        if args.channels == 'all':
+            nblocks = math.ceil(nchan / args.channel_chunks)
+            for block_idx in range(nblocks):
+                line = (
+                    f"python tclean.py {ms_paths_str} "
+                    f"--spw {spw} --channel_number {block_idx} "
+                    f"--nchannels {args.channel_chunks} "
+                    f"{tclean_opts}"
+                )
+                lines.append(line)
+        else:
+            channels = parse_channels(args.channels, nchan)
+            for chan in channels:
+                block_idx = chan // args.channel_chunks
+                line = (
+                    f"python tclean.py {ms_paths_str} "
+                    f"--spw {spw} --channel_number {block_idx} "
+                    f"--nchannels {args.channel_chunks} "
+                    f"{tclean_opts}"
+                )
+                lines.append(line)
 
     with open(args.output, 'w') as f:
         f.write('\n'.join(lines) + '\n')
--- TACC_Scripts/tclean.py
+++ TACC_Scripts/tclean.py
@@ -7,8 +7,6 @@ import argparse
 from casatasks import tclean, exportfits, casalog
 logfile = casalog.logfile()
 
-from astropy.io import fits
-
 
 if __name__ == '__main__':
 
@@ -17,7 +15,9 @@ if __name__ == '__main__':
     "parameters need to be passed in to reconstruct the output image file name"
 
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('input_MS', type=str, help='File to split')
+    parser.add_argument('input_MS', type=str, nargs='+', help='Measurement Set(s) to image')
+    parser.add_argument('--imagename', type=str, default=None,
+                        help='Base name for output image (default: common prefix of input MS basenames)')
     parser.add_argument('--channel_number', type=int, help='Channel number to split')
     parser.add_argument('--nchannels', type=int, default=1, help='Number of channels to split')
     parser.add_argument('--field', type=int, default=1, help='Field index')
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     parser.add_argument('--gridder', type=str, default='standard', help='Gridder to use')
     parser.add_argument('--imsize', type=int, default=128, help='Image size in pixels')
     parser.add_argument('--cell', type=str, default='0.004arcsec', help='Cell size')
-    parser.add_argument('--stokes', type=str, default='I', help='Stokes parameters to image')
+    parser.add_argument('--stokes', type=str, default='IQ', help='Stokes parameters to image')
     parser.add_argument('--niter', type=int, default=0, help='Number of iterations')
     parser.add_argument('--usemask', type=str, default='user', help='Masking mode')
     parser.add_argument('--threshold', type=str, default='0.0mJy', help='Threshold for cleaning')
@@ -33,19 +33,24 @@ if __name__ == '__main__':
 
     #casalog.setlogfile('logs/{SLURM_JOB_NAME}_{SLURM_JOB_ID}.casa'.format(**os.environ))
 
-    msname = args.input_MS
-    # Assume SLURM_ARRAY_JOB_ID is passed in as --channel-number
-    outputvis = os.path.basename(msname).replace('.ms', f'_spw_{args.spw:02d}_channel_{args.channel_number:04d}.ms')
-    imagename = outputvis.strip('.ms')
+    # Determine imagename base
+    if args.imagename:
+        imagename_base = args.imagename
+    else:
+        basenames = [os.path.basename(ms).replace('.ms', '') for ms in args.input_MS]
+        imagename_base = os.path.commonprefix(basenames).rstrip('_')
 
-    chan = args.channel_number
-    chan_beg = chan * args.nchannels
-    chan_end = chan_beg + args.nchannels - 1
+    imagename = f"{imagename_base}_spw_{args.spw:02d}_channel_{args.channel_number:04d}"
 
-    retdict = tclean(vis=msname, imagename=imagename, imsize=args.imsize, cell=args.cell, specmode='mfs',
-           selectdata=True, field=str(args.field), spw=f"{args.spw}:{chan_beg}~{chan_end}", usemask=args.usemask,
-           stokes=args.stokes,gridder=args.gridder, niter=args.niter, threshold=args.threshold, parallel=False,
-	   fullsummary=True)
+    chan_beg = args.channel_number * args.nchannels
+
+    retdict = tclean(vis=args.input_MS, imagename=imagename, imsize=args.imsize,
+            cell=args.cell, specmode='cube', selectdata=True,
+            field=str(args.field), spw=f"{args.spw}", start=f"{chan_beg}",
+            nchan=f"{args.nchannels}", width=1, outframe="LSRK",
+            interpolation='nearest', usemask=args.usemask,
+            stokes=args.stokes,gridder=args.gridder, niter=args.niter,
+            threshold=args.threshold, parallel=False, fullsummary=True)
 
     np.save(imagename + '_retdict.npy', retdict)
 
@@ -57,8 +62,10 @@ if __name__ == '__main__':
     exts = ['.psf', '.residual', '.sumwt', '.weight', '.pb', '.image', '.model', '.ms', '.mask']
     for ext in exts:
         imname = imagename + ext
-        if os.path.exists(imname):
+        if os.path.isdir(imname):
             shutil.rmtree(imname)
+        elif os.path.isfile(imname):
+            os.remove(imname)
 
     #if os.path.exists(imagename+'.fits'):
     #    os.remove(imagename+'.fits')
